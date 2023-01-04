d0bbba4e0e975426fd90a36536724c119d595764
Fabricio Magnus <fabricio.magnus92@outlook.com>
2023-01-04 14:51:31 -0300

    first commit

---
+++ src/Meetups/MeetUpGrid.svelte
@@ -0,0 +1,33 @@
+<script>
+  import MeetUpItem from "./MeetUpItem.svelte";
+
+  export let meetups;
+</script>
+
+<section class="meetups">
+  {#each meetups as m (m.id)}
+    <MeetUpItem
+      title={m.title}
+      description={m.description}
+      subtitle={m.subtitle}
+      address={m.address}
+      imageUrl={m.imageUrl}
+      email={m.contactEmail}
+    />
+  {/each}
+</section>
+
+<style>
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
